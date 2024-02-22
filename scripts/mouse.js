2e0e1393e38a7e67d64c738c947a2ba5816c1a19
minsu-l <jooo.minchan@gmail.com>
2024-02-22 16:19:18 +0900

    hide mouse when inactive

---
+++ scripts/mouse.js
@@ -0,0 +1,18 @@
+var mouseTimer = null, cursorVisible = true;
+
+    function disappearCursor() {
+        mouseTimer = null;
+        document.body.style.cursor = "none";
+        cursorVisible = false;
+    }
+
+    document.onmousemove = function() {
+        if (mouseTimer) {
+            window.clearTimeout(mouseTimer);
+        }
+        if (!cursorVisible) {
+            document.body.style.cursor = "default";
+            cursorVisible = true;
+        }
+        mouseTimer = window.setTimeout(disappearCursor, 3000);
+    };
